4ac1608a91b6d234d53c000b8ab034ae48dfebe1
bbreaker <breakerb@gmail.com>
2017-11-29 12:16:28 -0600

    added part functions

---
+++ Rmisc/runPART.R
@@ -0,0 +1,138 @@
+na2miss <- function (x, to = -99999) {
+  if (inherits(x, "factor")) {
+    levs <- c(levels(x), as.character(to))
+    x <- as.vector(x)
+    x[is.na(x)] <- to
+    return(factor(x, levels = levs))
+  }
+  x[is.na(x)] <- to
+  return(x)
+}
+
+eventNum <- function (event, reset = FALSE, na.fix = FALSE) {
+  event[is.na(event)] <- na.fix
+  event.rle <- rle(c(event, FALSE))
+  number <- 0L
+  ret.val <- rep(0L, length(event) + 1L)
+  i <- 1L
+  beg = 1L
+  while (i < length(event.rle$values)) {
+    if (event.rle$values[i]) {
+      number <- number + 1L
+      end <- beg + event.rle$lengths[i] + event.rle$lengths[i + 
+                                                              1] - 1L
+      ret.val[beg:end] <- number
+      beg <- end + 1L
+      i <- i + 2L
+    }
+    else {
+      beg <- event.rle$lengths[i] + 1L
+      i <- i + 1L
+    }
+  }
+  ret.val <- ret.val[seq(along = event)]
+  if (reset) 
+    ret.val <- ifelse(event, ret.val, 0L)
+  return(ret.val)
+}
+
+runPART <- function (flow, dates, start = NULL, end = NULL, drnArea) {
+  if (is.null(Start)) 
+    start <- dates[1L]
+  else if (is.character(start)) 
+    start <- as.Date(start)
+  if (is.null(end)) 
+    end <- dates[length(dates)]
+  else if (is.character(end)) 
+    end <- as.Date(end)
+  sel <- (dates >= start) & (dates <= end)
+  dates <- dates[sel]
+  flow <- pmax(flow[sel], 1e-99)
+  if (any(is.na(flow))) {
+    retVec <- NA
+  } else if (any(diff(as.double(dates)) != 1)) {
+    retVec <- NA
+  } else {
+    Nact <- max(drnArea^0.2, 1)
+    N <- as.integer(ceiling(Nact))
+    NF <- max(N - 1L, 1L)
+    NC <- max(N, 2L)
+    NC1 <- NC + 1L
+    ALLGWF <- ALLGWC <- ALLGWC1 <- rep(FALSE, length(flow))
+    BaseQF <- BaseQC <- BaseQC1 <- rep(NA_real_, length(flow))
+    DiffQ <- c(0, diff(flow))
+    AnteF <- na2miss(filter(DiffQ <= 0, rep(1, NF), sides = 1), 
+                     0)
+    AnteC <- na2miss(filter(DiffQ <= 0, rep(1, NC), sides = 1), 
+                     0)
+    AnteC1 <- na2miss(filter(DiffQ <= 0, rep(1, NC1), sides = 1), 
+                      0)
+    ALLGWF <- ifelse(AnteF == NF, TRUE, ALLGWF)
+    BaseQF <- ifelse(ALLGWF, Flow, BaseQF)
+    ALLGWC <- ifelse(AnteC == NC, TRUE, ALLGWC)
+    BaseQC <- ifelse(ALLGWC, Flow, BaseQC)
+    ALLGWC1 <- ifelse(AnteC1 == NC1, TRUE, ALLGWC1)
+    BaseQC1 <- ifelse(ALLGWC1, Flow, BaseQC1)
+    CkQ <- (flow > 1e-09) & (flow/shiftData(flow, k = -1, fill = 1) > 
+                               1.258925)
+    ALLGWF <- ifelse(ALLGWF & CkQ, FALSE, ALLGWF)
+    ALLGWC <- ifelse(ALLGWC & CkQ, FALSE, ALLGWC)
+    ALLGWC1 <- ifelse(ALLGWC1 & CkQ, FALSE, ALLGWC1)
+    Seq <- seq(length(flow))
+    BaseQF <- exp(approx(Seq[ALLGWF], log(Flow[ALLGWF]), xout = Seq, 
+                         rule = 2)$y)
+    BaseQC <- exp(approx(Seq[ALLGWC], log(Flow[ALLGWC]), xout = Seq, 
+                         rule = 2)$y)
+    BaseQC1 <- exp(approx(Seq[ALLGWC1], log(Flow[ALLGWC1]), xout = Seq, 
+                          rule = 2)$y)
+    while (any(CkQ <- (BaseQF > flow + 1e-06))) {
+      CkQ <- CkQ & !ALLGWF
+      Ck0 <- eventNum(!ALLGWF, reset = TRUE)
+      CkE <- unique(Ck0[CkQ])
+      for (i in CkE) {
+        Sel <- which(Ck0 == i)
+        MaxR <- BaseQF[Sel]/flow[Sel]
+        Pck <- which.max(MaxR)
+        ALLGWF[Sel[Pck]] <- TRUE
+        BaseQF[Sel[Pck]] <- flow[Sel[Pck]]
+      }
+      BaseQF <- exp(approx(Seq[ALLGWF], log(flow[ALLGWF]), 
+                           xout = Seq, rule = 2)$y)
+      BaseQF <- ifelse(BaseQF < 1e-06, 0, BaseQF)
+    }
+    while (any(CkQ <- (BaseQC > flow + 1e-06))) {
+      CkQ <- CkQ & !ALLGWC
+      Ck0 <- eventNum(!ALLGWC, reset = TRUE)
+      CkE <- unique(Ck0[CkQ])
+      for (i in CkE) {
+        Sel <- which(Ck0 == i)
+        MaxR <- BaseQC[Sel]/flow[Sel]
+        Pck <- which.max(MaxR)
+        ALLGWC[Sel[Pck]] <- TRUE
+        BaseQC[Sel[Pck]] <- flow[Sel[Pck]]
+      }
+      BaseQC <- exp(approx(Seq[ALLGWC], log(flow[ALLGWC]), 
+                           xout = Seq, rule = 2)$y)
+      BaseQC <- ifelse(BaseQC < 1e-06, 0, BaseQC)
+    }
+    while (any(CkQ <- (BaseQC1 > flow + 1e-06))) {
+      CkQ <- CkQ & !ALLGWC1
+      Ck0 <- eventNum(!ALLGWC1, reset = TRUE)
+      CkE <- unique(Ck0[CkQ])
+      for (i in CkE) {
+        Sel <- which(Ck0 == i)
+        MaxR <- BaseQC1[Sel]/Flow[Sel]
+        Pck <- which.max(MaxR)
+        ALLGWC1[Sel[Pck]] <- TRUE
+        BaseQC1[Sel[Pck]] <- flow[Sel[Pck]]
+      }
+      BaseQC1 <- exp(approx(Seq[ALLGWC1], log(flow[ALLGWC1]), 
+                            xout = Seq, rule = 2)$y)
+      BaseQC1 <- ifelse(BaseQC1 < 1e-06, 0, BaseQC1)
+    }
+    Ffact <- NC - Nact
+    BaseQ <- BaseQF * Ffact + BaseQC * (1 - Ffact)
+    retVec <- round(BaseQ, 3L)
+  }
+  return(retVec)
+}
