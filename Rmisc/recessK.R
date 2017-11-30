9b222cf5d5d47a673959b23470a5088fc7fbe640
bbreaker <breakerb@gmail.com>
2017-11-30 09:39:45 -0600

    updated recessK

--- Rmisc/recessK.R
+++ Rmisc/recessK.R
@@ -24,8 +24,25 @@ recessK <- function(flow, dates) {
     
     testRleDF <- testRle %>%
       mutate(qualK = if_else(vals != "fall", 0, 
-                             if_else(lengths < limVal, 0, 1)))
+                             if_else(lengths < limVal, 0, 1))) %>% 
+      group_by(qualK) %>% 
+      mutate(Index = ifelse(qualK == 0, 0, 1:n())) %>% 
+      data.frame()
     
+    retVal <- as.numeric()
+
+    for (i in seq(1, nrow(dplyr::filter(testRleDF, qualK == 1)), 1)) {
+      
+      chunk <- testRleDF[(which(testRleDF$Index == i) - 1):which(testRleDF$Index == i), ]
+      
+      chunk <- testDF[(chunk[1, 3] + 1):chunk[2, 3], ]
+      
+      retVal <- append(retVal, coef(lm(flow ~ dates, data = chunk))[2])
+      
+    }
     
   }
+  
+  return(signif(mean(retVal, na.rm = TRUE), 3))
+  
 }
