01db491387d99ce13ef6122d55a006c229890e8c
bbreaker <breakerb@gmail.com>
2017-11-30 08:08:37 -0600

    updated recessK

--- Rmisc/recessK.R
+++ Rmisc/recessK.R
@@ -1,13 +1,31 @@
 recessK <- function(flow, dates) {
+  
+  library(dplyr)
+  
   if (any(is.na(flow))) {
+    
     retVal <- NA
+    
   } else {
+    
     testDF <- data.frame(dates = dates, flow = flow)
+    
     testDF$diffQ <- c(0, diff(flow)) 
-    testDF$diffLog <- if_else(testDF$diffQ < 0, "a", "b")
+    
+    testDF$diffLog <- if_else(testDF$diffQ < 0, "fall", "notFall")
+    
     testRle <- data.frame(lengths = rle(testDF$diffLog)$lengths,
                           vals = rle(testDF$diffLog)$values)
-    testRle$cumVal <- cumsum(testRle$lengths)
-    limVal <- quantile(testRle[which(testRle$vals == "a"), 1], probs = 0.97)
     
+    testRle$cumVal <- cumsum(testRle$lengths)
+    
+    limVal <- quantile(testRle[which(testRle$vals == "fall"), 1], 
+                       probs = 0.99)
+    
+    testRleDF <- testRle %>%
+      mutate(qualK = if_else(vals != "fall", 0, 
+                             if_else(lengths < limVal, 0, 1)))
+    
+    
+  }
 }
