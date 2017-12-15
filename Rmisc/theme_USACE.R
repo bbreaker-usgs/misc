8539850a37d1e13f3aac8816ef06a551763434f6
Brian Breaker <breakerb@gmail.com>
2017-12-15 07:20:53 -0600

    Update theme_USACE.R

--- Rmisc/theme_USACE.R
+++ Rmisc/theme_USACE.R
@@ -1,16 +1,19 @@
 theme_USACE <-  function(base_size = 8){theme(
   text = element_text(family = 'serif', color = 'black'),
-  line = element_line(colour = 'black', size = 0.5 * 0.352778), # convert from pt to mm
-  rect = element_rect(colour = 'black', size = 0.5 * 0.352778),
+  line = element_line(colour = 'black', size = 0.2), 
+  rect = element_rect(colour = 'black', size = 0.2),
   plot.title = element_text(vjust = 3, size = 9),
   plot.margin = unit(c(1,1,1,1), 'lines'),
   panel.border = element_rect(fill = F),
-  #panel.grid.major = element_blank(),
-  #panel.grid.minor = element_blank(),
-  #panel.background = element_rect(fill = 'white'),
+  panel.grid.major = element_line(colour = 'grey50', size = 0.2),
+  panel.grid.minor = element_line(colour = 'grey75', size = 0.1),
+  panel.background = element_rect(fill = 'white'),
   #defaults legend to upper left, can/should be overridden based on graph
   legend.background = element_blank(),
   legend.justification = c("left", "top"),
+  # this value should be adjusted dependent on 
+  # graph with the addition of another 
+  # theme(legend.position = c(X, Y)) argument after theme_USACE()
   legend.position = c(0.8, 0.5),
   legend.key = element_blank(),
   legend.title = element_text(size = 9),
